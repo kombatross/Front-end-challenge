08b1210316e35ea647829427e922f883fd09ccad
Kombatross <konradnater1@wp.pl>
2021-03-15 16:24:48 +0100

    Scroll % details

--- elscripto.js
+++ elscripto.js
@@ -9,8 +9,13 @@ async function name_albums() {
     for (i = 0; i < 100; i++) {
         var choice = entry[i];
         var titles = choice['im:name'].label;
-        document.getElementById("Titles_place1").innerHTML += '<li> <a href=#' + titles + '>' + titles + ' <br> <br> ';
-        document.getElementById("Titles_place").innerHTML += '<a name=' + titles + '>'+ titles + '<br> <br> <div id="Photo_place'+[i]+'"></div>  <div class="dropdown-divider"></div> ';    }
+        var artist = choice['im:artist'].label;
+        var category = choice.category.attributes.label;
+        var release = choice['im:releaseDate'].attributes.label;
+        console.log(category);
+        document.getElementById("Titles_place1").innerHTML += '<li> <a href=#' + titles + '>' + titles + ' - ' + artist + ' <br> <br> ';
+        document.getElementById("Titles_place").innerHTML += '<a name=' + titles + '> <b>' + titles + '</b> - ' + artist + '<br> Release Date: ' + release + '<div class="d-flex flex-row-reverse justify-content-end" id="Photo_place' + [i] + '"><p></p>  </div><div class="dropdown-divider"></div > ';
+    }
 }
 name_albums();
 
@@ -22,11 +27,9 @@ async function photo_albums() {
     for (i = 0; i < 100; i++) {
         var choice = entry[i];
         var photos = choice['im:image'][2].label;
-        document.getElementById("Photo_place"+[i]).innerHTML += '<img src="'+photos+'">';
+        document.getElementById("Photo_place" + [i]).innerHTML += '<img src="' + photos + '">';
         console.log(photos);
 
     }
 }
 photo_albums();
-
-
